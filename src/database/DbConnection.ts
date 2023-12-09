25e5b208e91cfb77b16505bd86ccc0432a79060b
Jos Warmer <jos.warmer@openmodeling.nl>
2023-12-09 09:26:37 +0100

    Fix postgres parameters

--- src/database/DbConnection.ts
+++ src/database/DbConnection.ts
@@ -10,7 +10,7 @@ const port = parseInt( process.env.PGPORT || "5432", 10 );
 export const config = {
     database: process.env.PGDATABASE || "lionweb_test",
     host: process.env.PGHOST || "postgres",
-    post: port,
+    port: port,
     user: process.env.PGUSER || "postgres",
     password: process.env.PGPASSWORD || "lionweb"
 };
@@ -18,6 +18,5 @@ export const config = {
 console.log("POSTGRES CONFIG: " + JSON.stringify(config, null, 2))
 
 const pgp = pgPromise();
-console.log("config " + JSON.stringify(config, null, 2))
 export const db = pgp( config );
 console.log("next again")
