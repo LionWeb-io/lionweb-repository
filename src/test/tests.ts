962267d0beba48515a52d9b6344df7ebbc5177e3
Jos Warmer <jos.warmer@openmodeling.nl>
2023-12-11 20:05:37 +0100

    Split into different tests

--- src/test/tests.ts
+++ src/test/tests.ts
@@ -1,33 +1,34 @@
 import { LionWebJsonChunk, LionWebJsonNode } from "@lionweb/validation";
 import {assert} from "chai"
+import { before } from "mocha";
 import { LionWebJsonDiff } from "./diff/LionWebJsonDiff.js";
 import { TestClient } from "./TestClient.js";
 const {deepEqual} = assert
 
 describe("Library test model", () => {
 
-    it("[de-]serialize example library", async () => {
-        const t = new TestClient();
-        const jsonModel = t.readModel("./src/test/data/Disk_1.json") as LionWebJsonChunk;
-        await t.testStore();
-        const retrieve = await t.testRetrieve() as LionWebJsonChunk;
-        // console.log("  ");
-        // console.log("JsonModel: " + JSON.stringify(jsonModel));
-        // console.log("  ");
-        // console.log("Retrieved: " + JSON.stringify(retrieve));
+    const t = new TestClient();
+    let jsonModel: LionWebJsonChunk;
 
+    beforeEach(function (done) {
+        jsonModel = t.readModel("./src/test/data/Disk_1.json") as LionWebJsonChunk;
+        t.testStore();
+        done();
+    });
+
+    it("store and retrieve nodes", async () => {
+        const retrieve = await t.testRetrieve() as LionWebJsonChunk;
         const diff = new LionWebJsonDiff();
         diff.diffLwChunk(jsonModel, retrieve);
-        // No errors expected, just print
-        // console.log("Errors " + JSON.stringify(diff.errors, null, 2))
         deepEqual(diff.errors, [])
-        
-        // Quick test whether thye root node is returned by "partitions" call
-        jsonModel.nodes = jsonModel.nodes.filter(node => node.parent === null);
+    })
+
+    it("retrieve partitions", async () => {
+        const model = structuredClone(jsonModel);
+        model.nodes = model.nodes.filter(node => node.parent === null);
         const partitions = await t.testPartitions();
-        diff.diffLwChunk(jsonModel, partitions);
-        // No errors expected, just print
-        console.log("Errors " + JSON.stringify(diff.errors, null, 2))
+        const diff = new LionWebJsonDiff();
+        diff.diffLwChunk(model, partitions);
         deepEqual(diff.errors, [])
     })
     
