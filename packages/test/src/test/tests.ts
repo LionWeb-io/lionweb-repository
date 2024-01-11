29eec690c374d963b141facbc201e461499b25f3
Jos Warmer <jos.warmer@openmodeling.nl>
2024-01-11 21:06:13 +0100

    Further cleanup

--- packages/test/src/test/tests.ts
+++ packages/test/src/test/tests.ts
@@ -39,96 +39,162 @@ describe("Repository tests", () => {
 
     describe("Move node (9) to from parent (4) to (5)", () => {
         it("test update full partition", async () => {
-            await testResult("./src/test/data/move-child/Disk-move-child-partition.json", "./src/test/data/move-child/Disk-move-child-partition.json")
+            await testResult(
+                "./src/test/data/move-child/Disk-move-child-partition.json",
+                "./src/test/data/move-child/Disk-move-child-partition.json"
+            )
         })
         it("test update node (5)", async () => {
-            await testResult("./src/test/data/move-child/Disk-move-child-partition.json", "./src/test/data/move-child/Disk-move-child-single-node.json")
+            await testResult(
+                "./src/test/data/move-child/Disk-move-child-partition.json",
+                "./src/test/data/move-child/Disk-move-child-single-node.json"
+            )
         })
         it("test update nodes (5) and (4)", async () => {
-            await testResult("./src/test/data/move-child/Disk-move-child-partition.json","./src/test/data/move-child/Disk-move-child-two-nodes.json")
+            await testResult(
+                "./src/test/data/move-child/Disk-move-child-partition.json",
+                "./src/test/data/move-child/Disk-move-child-two-nodes.json"
+            )
         })
 
         it("test update nodes (5) and (9)", async () => {
-            await testResult("./src/test/data/move-child/Disk-move-child-partition.json","./src/test/data/move-child/Disk-move-child-two-nodes-2.json")
+            await testResult(
+                "./src/test/data/move-child/Disk-move-child-partition.json",
+                "./src/test/data/move-child/Disk-move-child-two-nodes-2.json"
+            )
         })
     })
 
     describe("Change value of node (3) property 'name' to 'root-new-value'", () => {
         it("test update full partition", async () => {
-            await testResult("./src/test/data/change-property-value/Disk_Property_value_changed-partition.json", "./src/test/data/change-property-value/Disk_Property_value_changed-partition.json")
+            await testResult(
+                "./src/test/data/change-property-value/Disk_Property_value_changed-partition.json",
+                "./src/test/data/change-property-value/Disk_Property_value_changed-partition.json"
+            )
         })
         it("test update node (3)", async () => {
-            await testResult("./src/test/data/change-property-value/Disk_Property_value_changed-partition.json", "./src/test/data/change-property-value/Disk_Property_value_changed-single-node.json")
+            await testResult(
+                "./src/test/data/change-property-value/Disk_Property_value_changed-partition.json",
+                "./src/test/data/change-property-value/Disk_Property_value_changed-single-node.json"
+            )
         })
     })
 
     describe("Add new property ", () => {
         it("test update full partition", async () => {
-            await testResult("./src/test/data/add-new-property-with-value/Disk-Property-add-property-partition.json", "./src/test/data/add-new-property-with-value/Disk-Property-add-property-partition.json")
+            await testResult(
+                "./src/test/data/add-new-property-with-value/Disk-Property-add-property-partition.json",
+                "./src/test/data/add-new-property-with-value/Disk-Property-add-property-partition.json"
+            )
         })
         it("test update single node", async () => {
-            await testResult("./src/test/data/add-new-property-with-value/Disk-Property-add-property-partition.json", "./src/test/data/add-new-property-with-value/Disk-Property-add-property-single-node.json")
+            await testResult(
+                "./src/test/data/add-new-property-with-value/Disk-Property-add-property-partition.json",
+                "./src/test/data/add-new-property-with-value/Disk-Property-add-property-single-node.json"
+            )
         })
     })
 
     describe("Remove node (4) from parent (3) and mode child (9) to (5)", () => {
         it("test update full partition", async () => {
-            await testResult("./src/test/data/remove-child/Disk-remove-child-partition.json", "./src/test/data/remove-child/Disk-remove-child-partition.json")
+            await testResult(
+                "./src/test/data/remove-child/Disk-remove-child-partition.json",
+                "./src/test/data/remove-child/Disk-remove-child-partition.json"
+            )
         })
         it("test update (3)", async () => {
-            await testResult("./src/test/data/remove-child/Disk-remove-child-partition.json", "./src/test/data/remove-child/Disk-remove-child-single-node.json")
+            await testResult(
+                "./src/test/data/remove-child/Disk-remove-child-partition.json",
+                "./src/test/data/remove-child/Disk-remove-child-single-node.json"
+            )
         })
     })
 
     describe("Add reference", () => {
         it("test update full partition", async () => {
-            await testResult("./src/test/data/add-reference/Disk_add-reference-partition.json", "./src/test/data/add-reference/Disk_add-reference-partition.json")
+            await testResult(
+                "./src/test/data/add-reference/Disk_add-reference-partition.json",
+                "./src/test/data/add-reference/Disk_add-reference-partition.json"
+            )
         })
         it("test update single node", async () => {
-            await testResult("./src/test/data/add-reference/Disk_add-reference-partition.json", "./src/test/data/add-reference/Disk_add-reference-single-node.json")
+            await testResult(
+                "./src/test/data/add-reference/Disk_add-reference-partition.json",
+                "./src/test/data/add-reference/Disk_add-reference-single-node.json"
+            )
         })
     })
     describe("Remove reference", () => {
         it("test update full partition", async () => {
-            await testResult("./src/test/data/remove-reference/Disk-remove-reference-partition.json", "./src/test/data/remove-reference/Disk-remove-reference-partition.json")
+            await testResult(
+                "./src/test/data/remove-reference/Disk-remove-reference-partition.json",
+                "./src/test/data/remove-reference/Disk-remove-reference-partition.json"
+            )
         })
         it("test update single node", async () => {
-            await testResult("./src/test/data/remove-reference/Disk-remove-reference-partition.json", "./src/test/data/remove-reference/Disk-remove-reference-single-node.json")
+            await testResult(
+                "./src/test/data/remove-reference/Disk-remove-reference-partition.json",
+                "./src/test/data/remove-reference/Disk-remove-reference-single-node.json"
+            )
         })
     })
     describe("Remove annotation", () => {
         // TODO ANN-1 is still in the repo !!!
         it("test update full partition", async () => {
-            await testResult("./src/test/data/remove-annotation/Disk-remove-annotation-partition.json", "./src/test/data/remove-annotation/Disk-remove-annotation-partition.json")
+            await testResult(
+                "./src/test/data/remove-annotation/Disk-remove-annotation-partition.json",
+                "./src/test/data/remove-annotation/Disk-remove-annotation-partition.json"
+            )
         })
         it("test update single node", async () => {
-            await testResult("./src/test/data/remove-annotation/Disk-remove-annotation-partition.json", "./src/test/data/remove-annotation/Disk-remove-annotation-single-node.json")
+            await testResult(
+                "./src/test/data/remove-annotation/Disk-remove-annotation-partition.json",
+                "./src/test/data/remove-annotation/Disk-remove-annotation-single-node.json"
+            )
         })
     })
     describe("Add new annotation", () => {
         // TODO ANN-1 is still in the repo !!!
         it("test update full partition", async () => {
-            await testResult("./src/test/data/add-new-annotation/Disk-add-new-annotation-partition.json", "./src/test/data/add-new-annotation/Disk-add-new-annotation-partition.json")
+            await testResult(
+                "./src/test/data/add-new-annotation/Disk-add-new-annotation-partition.json",
+                "./src/test/data/add-new-annotation/Disk-add-new-annotation-partition.json"
+            )
         })
         it("test update two nodes node", async () => {
-            await testResult("./src/test/data/add-new-annotation/Disk-add-new-annotation-partition.json", "./src/test/data/add-new-annotation/Disk-add-new-annotation-two-nodes.json")
+            await testResult(
+                "./src/test/data/add-new-annotation/Disk-add-new-annotation-partition.json",
+                "./src/test/data/add-new-annotation/Disk-add-new-annotation-two-nodes.json"
+            )
         })
     })
     describe("Add new node", () => {
         // TODO ANN-1 is still in the repo !!!
         it("test update full partition", async () => {
-            await testResult("./src/test/data/add-new-nodes/Disk-add-new-nodes-partition.json", "./src/test/data/add-new-nodes/Disk-add-new-nodes-partition.json")
+            await testResult(
+                "./src/test/data/add-new-nodes/Disk-add-new-nodes-partition.json",
+                "./src/test/data/add-new-nodes/Disk-add-new-nodes-partition.json"
+            )
         })
         it("test update single node", async () => {
-            await testResult("./src/test/data/add-new-nodes/Disk-add-new-nodes-partition.json", "./src/test/data/add-new-nodes/Disk-add-new-nodes-single-node.json")
+            await testResult(
+                "./src/test/data/add-new-nodes/Disk-add-new-nodes-partition.json",
+                "./src/test/data/add-new-nodes/Disk-add-new-nodes-single-node.json"
+            )
         })
     })
     describe("Reorder children", () => {
         it("test update full partition", async () => {
-            await testResult("./src/test/data/reorder-children/reorder-children-partition.json", "./src/test/data/reorder-children/reorder-children-partition.json")
+            await testResult(
+                "./src/test/data/reorder-children/reorder-children-partition.json",
+                "./src/test/data/reorder-children/reorder-children-partition.json"
+            )
         })
         it("test update single node", async () => {
-            await testResult("./src/test/data/reorder-children/reorder-children-partition.json", "./src/test/data/reorder-children/reorder-children-single-node.json")
+            await testResult(
+                "./src/test/data/reorder-children/reorder-children-partition.json",
+                "./src/test/data/reorder-children/reorder-children-single-node.json"
+            )
         })
     })
 
