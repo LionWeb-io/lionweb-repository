9cf29bf5acb8e2c647851f75cfd84cd882c9dce9
Federico Tomassetti <federico@strumenta.com>
2024-03-07 13:27:09 +0100

    adding possibility to configure a token

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import http from "http"
-import express, { Express } from "express"
+import express, {Express, NextFunction, Response, Request} from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 import { dbConnection, pgp } from "./DbConnection.js"
@@ -9,6 +9,7 @@ import { registerInspection } from "@lionweb/repository-inspection"
 import { registerBulkApi } from "@lionweb/repository-bulkapi"
 import { registerAdditionalApi } from "@lionweb/repository-additionalapi"
 import { registerLanguagesApi } from "@lionweb/repository-languages"
+import { HttpClientErrors } from "@lionweb/repository-common"
 
 dotenv.config()
 
@@ -27,6 +28,23 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({limit: process.env.BODY_LIMIT || '50mb'}))
 
+const expectedToken = process.env.EXPECTED_TOKEN
+
+function verifyToken(req: Request, res: Response, next: NextFunction) {
+    if (expectedToken != null) {
+        const providedToken = req.headers['authorization']
+        if (providedToken == null || typeof providedToken !== "string" || providedToken.trim() != expectedToken) {
+            return res.status(HttpClientErrors.Unauthorized).send("Invalid token or no token provided")
+        } else {
+            next();
+        }
+    } else {
+        next()
+    }
+}
+
+app.use(verifyToken)
+
 registerDBAdmin(app, dbConnection, pgp)
 registerBulkApi(app, dbConnection, pgp)
 registerInspection(app, dbConnection, pgp)
@@ -36,6 +54,13 @@ registerLanguagesApi(app, dbConnection, pgp)
 const httpServer = http.createServer(app)
 
 const serverPort = parseInt(process.env.NODE_PORT || "3005")
+
 httpServer.listen(serverPort, () => {
     console.log(`Server is running at port ${serverPort} =========================================================`)
+    if (expectedToken == null) {
+        console.log("WARNING! The server is not protected by a token. It can be accessed freely. " +
+            "If that is NOT your intention act accordingly.")
+    } else if (expectedToken.length < 24) {
+        console.log("WARNING! The used token is quite short. Consider using a token of 24 characters or more.")
+    }
 })
