f5bb50800ba1d79dca946a5a50f22a7e88caee1d
Federico Tomassetti <federico@strumenta.com>
2024-04-26 10:56:16 +0200

    Use Buffer to serialize long lists of IDs

--- packages/inspection/src/controllers/InspectionApi.ts
+++ packages/inspection/src/controllers/InspectionApi.ts
@@ -1,5 +1,6 @@
 import e, { Request, Response } from "express"
 import { InspectionContext } from "../main.js";
+import {ClassifierNodes, LanguageNodes} from "../database/InspectionApiWorker";
 
 export interface InspectionApi {
     nodesByClassifier(request: Request, response: Response): void
@@ -7,20 +8,99 @@ export interface InspectionApi {
     nodesByLanguage(request: Request, response: Response): void
 }
 
+/**
+ * We use this class to track a cursor over a Buffer and to
+ * allocate a new one when necessary.
+ */
+class BufferHolder {
+    private buffer = Buffer.alloc(1000000)
+    private usedSize: number = 0
+    getBuffer() {
+        // We return only the part of the buffer actually filled with data
+        const res = this.buffer.subarray(0, this.usedSize)
+        return res
+    }
+
+    write(string: string) {
+        const stringLength = Buffer.byteLength(string)
+        const remainingSize = this.buffer.length - this.usedSize
+        if (remainingSize < stringLength) {
+            const oldBuffer = this.buffer
+            this.buffer = Buffer.alloc(oldBuffer.length * 2)
+            oldBuffer.copy(this.buffer)
+            // Perhaps we need to grow again the buffer, so we make
+            // another call to this method to re-check the size
+            return this.write(string)
+        } else {
+            this.buffer.write(string, this.usedSize)
+            this.usedSize += stringLength
+        }
+    }
+}
+
 class InspectionApiImpl implements InspectionApi {
     constructor(private context: InspectionContext) {
+    }
+
+    private serializeInBufferIDsList(buffer: BufferHolder, ids: string[], limit: number) {
+        const idsToSend = (limit < 0 || limit >= ids.length) ? ids : ids.slice(0, limit)
+        idsToSend.forEach((id, index)=>{
+            if (index != 0) {
+                buffer.write(",\n")
+            }
+            buffer.write(`"${id}"`)
+        })
+    }
+
+    private classifierNodesToBuffer(classifierNodes: ClassifierNodes[], limit: number): Buffer {
+        const bufferHolder = new BufferHolder()
+        bufferHolder.write("[")
+        classifierNodes.forEach((element, index)=>{
+            if (index != 0) {
+                bufferHolder.write(",")
+            }
+            bufferHolder.write(`{"language"="${element.language}",`)
+            bufferHolder.write(`"classifier"="${element.classifier}",`)
+            bufferHolder.write(`"ids"=[`)
+            this.serializeInBufferIDsList(bufferHolder, element.ids, limit)
+            bufferHolder.write(`], "size"=${element.size}}\n`)
+        })
+        bufferHolder.write("]")
+        return bufferHolder.getBuffer()
+    }
+
+    private languageNodesToBuffer(languageNodes: LanguageNodes[], limit: number): Buffer {
+        const bufferHolder = new BufferHolder()
+        bufferHolder.write("[")
+        languageNodes.forEach((element, index)=>{
+            if (index != 0) {
+                bufferHolder.write(",")
+            }
+            bufferHolder.write(`"language"="${element.language}","`)
+            bufferHolder.write(`"ids"=[`)
+            this.serializeInBufferIDsList(bufferHolder, element.ids, limit)
+            bufferHolder.write(`"], size"=${element.size}"`)
+        })
+        bufferHolder.write("]")
+        return bufferHolder.getBuffer()
     }
 
     nodesByClassifier = async (request: e.Request, response: e.Response)=> {
         const sql = this.context.inspectionQueries.nodesByClassifier();
         const queryResult = await this.context.inspectionApiWorker.nodesByClassifier(sql)
-        response.send(queryResult)
+        const limitStr = request.query.limit
+        // TODO handle the case in which multiple query parameters are specified
+        let limit: number = limitStr === undefined ? -1 : parseInt(limitStr as string, 10)
+        response.send(this.classifierNodesToBuffer(queryResult, limit))
     }
 
     nodesByLanguage = async (request: e.Request, response: e.Response) => {
         const sql = this.context.inspectionQueries.nodesByLanguage();
         const queryResult = await this.context.inspectionApiWorker.nodesByLanguage(sql)
-        response.send(queryResult)
+        const limitStr = request.query.limit
+        // TODO handle the case in which multiple query parameters are specified
+        let limit: number = limitStr === undefined ? -1 : parseInt(limitStr as string, 10)
+        response.send(this.languageNodesToBuffer(queryResult, limit))
     }
 }
 
