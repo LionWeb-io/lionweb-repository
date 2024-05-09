432f66ac76a24f25597ae88a432cec7e0a80f642
Jos Warmer <jos.warmer@openmodeling.nl>
2024-05-09 10:24:42 +0200

    Capture all database changes in one place, so we can avoid multiple updates to one row.

---
+++ packages/bulkapi/src/database/InitializedMapToArray.ts
@@ -0,0 +1,31 @@
+export class InitializedMapToArray<KEY, VALUE> {
+    /**
+     * Use the stringified version of the KEY, so two objects with the same contents are iodentical
+     */
+    private map: Map<string, VALUE[]> = new Map<string, VALUE[]>()
+    
+    constructor() {
+    }
+    
+    get(key: KEY): VALUE[] {
+        const keyString = JSON.stringify(key)
+        let existingValue = this.map.get(keyString)
+        if (existingValue === undefined) {
+           existingValue = []
+           this.map.set(keyString, existingValue) 
+        }
+        return existingValue
+    }
+    
+    add(key: KEY, value: VALUE) {
+        this.get(key).push(value)
+    }
+
+    values(): VALUE[][] {
+        return Array.from(this.map.values()) as VALUE[][]
+    }
+
+    entries(): [KEY, VALUE[]][] {
+        return Array.from(this.map.entries()) as [KEY, VALUE[]][]
+    }
+}
