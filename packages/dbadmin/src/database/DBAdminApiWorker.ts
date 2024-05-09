9a7ec516b98460f4fb444e9bb5027a50438fb29e
Jos Warmer <jos.warmer@openmodeling.nl>
2024-05-09 21:51:50 +0200

    Remove database.ts, as it is always executed, which it should not.

--- packages/dbadmin/src/database/DBAdminApiWorker.ts
+++ packages/dbadmin/src/database/DBAdminApiWorker.ts
@@ -1,4 +1,4 @@
-import { HttpSuccessCodes, logger, QueryReturnType } from "@lionweb/repository-common";
+import { HttpSuccessCodes, QueryReturnType } from "@lionweb/repository-common";
 import { DbAdminApiContext } from "../main.js";
 
 /**
@@ -11,12 +11,7 @@ export class DBAdminApiWorker {
 
     async init(sql: string): Promise<QueryReturnType<string>> {
         // const q = this.ctx.pgp.helpers.concat([sql])
-        logger.requestLog("INIT")
-        logger.requestLog(sql)
-        logger.requestLog("INIT2")
-        logger.requestLog(sql.replaceAll("\n", " HELLO JOS "))
-        logger.requestLog("INIT3")
-        const queryResult = await this.ctx.dbConnection.result(sql.replaceAll("\n", "    "))
+        const queryResult = await this.ctx.dbConnection.result(sql.replaceAll("\n", "            "))
         return { 
             status: HttpSuccessCodes.Ok,
             query: "",
