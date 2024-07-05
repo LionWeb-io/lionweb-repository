39d14dbc2a1beb08d2dfa9df05e77c24d6b29a94
Federico Tomassetti <federico@strumenta.com>
2024-07-05 08:58:40 +0200

    remove explicit reference to tablespace

--- packages/dbadmin/src/tools/create-database-sql.ts
+++ packages/dbadmin/src/tools/create-database-sql.ts
@@ -10,7 +10,6 @@ CREATE DATABASE ${ServerConfig.getInstance().pgDb()}
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8'
     LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
