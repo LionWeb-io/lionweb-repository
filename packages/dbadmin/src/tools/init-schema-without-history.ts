f4ee4c3d83703e7f7d4b5629005498250120b58a
Federico Tomassetti <federico@strumenta.com>
2024-07-24 17:13:55 +0200

    Wip

--- packages/dbadmin/src/tools/init-schema-without-history.ts
+++ packages/dbadmin/src/tools/init-schema-without-history.ts
@@ -30,6 +30,8 @@ export function initSchemaWithoutHistory(schema: string): string {
     -- DROP INDEX IF EXISTS ContainmentsNodesIndex;
     -- DROP INDEX IF EXISTS PropertiesNodesIndex;
     -- DROP INDEX IF EXISTS ReferencesNodesIndex;
+    -- DROP INDEX IF EXISTS MpsValuesIndex;
+    -- DROP INDEX IF EXISTS MpsIdIndex;
     
     -- Creates metapointers table
     CREATE TABLE IF NOT EXISTS ${METAPOINTERS_TABLE} (
@@ -114,7 +116,8 @@ export function initSchemaWithoutHistory(schema: string): string {
     -- CREATE INDEX PropertiesNodesIndex   ON ${PROPERTIES_TABLE}   (node_id)
     -- CREATE INDEX ReferencesNodesIndex   ON ${REFERENCES_TABLE}   (node_id)
     -- CREATE INDEX ReservedIdsIndex       ON ${RESERVED_IDS_TABLE} (node_id)
-    -- CREATE INDEX MpsIndex       ON ${METAPOINTERS_TABLE} (language, _version, key)
+    -- CREATE INDEX MpsValuesIndex       ON ${METAPOINTERS_TABLE} (language, _version, key)
+    -- CREATE INDEX MpsIdIndex       ON ${METAPOINTERS_TABLE} (id)
     
     -- SET repo.version = 0;
     -- SET repo.version = (SELECT value FROM CURRENT_DATA WHERE key = 'repo.version');
